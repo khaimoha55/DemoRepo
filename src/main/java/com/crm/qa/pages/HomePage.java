88ecc023bf507469548fa9d9a8da4bb4fba177e6
khaimoha55 <khaimoha55@gmail.com>
2019-01-14 16:56:44 +0000

    first

---
+++ src/main/java/com/crm/qa/pages/HomePage.java
@@ -0,0 +1,69 @@
+package com.crm.qa.pages;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.crm.qa.base.TestBase;
+
+public class HomePage extends TestBase {
+	
+	@FindBy(xpath="//td[contains(text(),'User: khaiyum')]")
+			WebElement userNameLabel1;
+	
+	@FindBy(xpath="//a[contains(text(),'Contacts')]")
+			WebElement contactsLink;
+	
+	@FindBy(xpath="//a[@title='New Contact']")
+	WebElement newContactLink;
+	
+	@FindBy(xpath="//a[contains(text(),'Deals')]")
+             WebElement DealsLink;
+	
+	@FindBy(xpath="//a[contains(text(),'Tasks')]")
+	WebElement TasksLink;
+	
+	//Initialize the pageobjects
+	
+	public HomePage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Actions
+	
+	public String verifyHomePageTitle() {
+		return driver.getTitle();
+		
+		}
+	
+	
+	public boolean verifyCorrectUserName() {
+		return userNameLabel1.isDisplayed();
+		
+	}
+	
+	public ContactsPage clickOnContactsLink() {
+		contactsLink.click();
+		return new ContactsPage();
+	}
+	
+	public void clickOnNewContactLink() {
+		Actions action = new Actions(driver);
+		action.moveToElement(contactsLink).build().perform();
+		newContactLink.click();
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
